b3f2ae1c61a9f7b608fbb01c2ac028ed250edbdd
Herlander Bento <herlanderbento19@gmail.com>
2023-01-09 16:53:42 +0100

    first commit: create setup for app

---
+++ src/styles/themes/default.ts
@@ -0,0 +1,20 @@
+export const defaultTheme = {
+  'white-default': '#ffffff',
+
+  'dark-default': '#1C1C1C',
+
+  'gray-100': '#E1E1E6',
+  'gray-300': '#C4C4CC',
+  'gray-400': '#8D8D99',
+  'gray-500': '#7C7C8A',
+  'gray-600': '#323238',
+  'gray-700': '#29292E',
+  'gray-800': '#202024',
+  'gray-900': '#121214',
+
+  'green-500': '#2BD0C9',
+
+  'blue-700': '#263C57',
+
+  'yellow-500': '#FBA94C',
+}
